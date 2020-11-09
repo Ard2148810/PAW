12073435676f71147e5f834d01f7a98e00c3cab7
hobogalaxy <zalewski.ukas@gmail.com>
2020-11-09 21:03:18 +0100

    Add auth and error interceptors

---
+++ client/src/app/services/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment} from '../../environments/environment';
+import { AuthenticationService} from './authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // add header with basic auth credentials if user is logged in and request is to the api url
+    const user = this.authenticationService.getCurrentUser();
+    const isLoggedIn = user && user.token;
+    const isApiUrl = request.url.startsWith(environment.backendURL);
+
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: {
+          token: `${user.token}`
+        }
+      });
+    }
+
+    console.log(request);
+
+    return next.handle(request);
+  }
+}
