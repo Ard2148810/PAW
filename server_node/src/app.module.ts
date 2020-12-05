4cbfa8fb0e6af4587815e2e139709e63386396d5
Nela <nelakacz123@gmail.com>
2020-12-05 17:52:26 +0100

    Connect to Mongo DB with Type ORM

--- server_node/src/app.module.ts
+++ server_node/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [UsersModule, BoardsModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
