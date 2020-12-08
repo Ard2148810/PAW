217bf9691aef2249899a8394e7f7858c3fc84f52
Adrian Ałaszewski <wns2148810@interia.pl>
2020-12-08 20:18:00 +0100

    List wildcard view

--- client/src/app/components/list/list.component.ts
+++ client/src/app/components/list/list.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ListComponent implements OnInit {
   @Input() title: string;
   @Input() id: string;
+  @Input() wildcard = false;
 
   constructor() { }
 
