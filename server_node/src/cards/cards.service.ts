22e8361e30cb2f1cfa5d4af7a14f4de05622a547
adrian61 <215777@edu.p.lodz.pl>
2020-12-09 14:14:22 +0100

    implemented services

--- server_node/src/cards/cards.service.ts
+++ server_node/src/cards/cards.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Card } from './entities/card.entity';
 
 @Injectable()
 export class CardsService {
+  constructor(
+    @InjectRepository(Card)
+    private readonly cardRepository: Repository<Card>,
+  ) {}
+
   create(createCardDto: CreateCardDto) {
     return 'This action adds a new card';
   }
