66c4adb85e5bb53c152bd3c12ddc88bfa30649de
adrian61 <215777@edu.p.lodz.pl>
2020-12-09 14:08:53 +0100

    fixed list controller

--- server_node/src/lists/lists.controller.ts
+++ server_node/src/lists/lists.controller.ts
@@ -1,18 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Put,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ListsService } from './lists.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
 
 @ApiTags('lists')
 @ApiBearerAuth()
@@ -21,6 +13,7 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 export class ListsController {
   constructor(private readonly listsService: ListsService) {}
 
+  @ApiBody({ type: CreateListDto })
   @Post()
   create(@Body() createListDto: CreateListDto) {
     return this.listsService.create(createListDto);
@@ -36,6 +29,7 @@ export class ListsController {
     return this.listsService.findOne(+id);
   }
 
+  @ApiBody({ type: UpdateListDto })
   @Put(':id')
   update(@Param('id') id: string, @Body() updateListDto: UpdateListDto) {
     return this.listsService.update(+id, updateListDto);
