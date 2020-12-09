db5fbe53bde24962801821308ca9b50dc51a308e
adrian61 <215777@edu.p.lodz.pl>
2020-12-09 13:41:22 +0100

    card entity

--- server_node/src/cards/entities/card.entity.ts
+++ server_node/src/cards/entities/card.entity.ts
@@ -1,1 +1,28 @@
-export class Card {}
+import {
+    Entity,
+    Column,
+    ObjectIdColumn,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Comment } from '../../comments/entities/comment.entity';
+
+@Entity()
+export class Card {
+    @PrimaryGeneratedColumn()
+    @ObjectIdColumn()
+    _id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    comments: [Comment]
+    @Column()
+    teamMembers: [string];
+
+    @Column({ type: 'timestamp' })
+    date: Date;
+}
