ac47f4a00da722c16c3124e167d0013da55a431b
adrian61 <215777@edu.p.lodz.pl>
2020-12-09 13:46:39 +0100

    list entity

--- server_node/src/lists/entities/list.entity.ts
+++ server_node/src/lists/entities/list.entity.ts
@@ -1,10 +1,17 @@
 import { Entity, Column, ObjectIdColumn } from 'typeorm';
+import { Card } from '../../cards/entities/card.entity';
 
 @Entity()
 export class List {
-  @ObjectIdColumn()
-  id: string;
+    @ObjectIdColumn()
+    id: string;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
+
+    @Column((type) => Card)
+    cards: Card[];
+
+    @Column()
+    position: BigInteger;
 }
