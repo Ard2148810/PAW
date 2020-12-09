dc66c63676b4079237b592fa6b1174dd6274704a
adrian61 <215777@edu.p.lodz.pl>
2020-12-09 13:44:03 +0100

    comment entity

--- server_node/src/comments/entities/comment.entity.ts
+++ server_node/src/comments/entities/comment.entity.ts
@@ -1,1 +1,23 @@
-export class Comment {}
+import {
+    Entity,
+    Column,
+    ObjectIdColumn,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn()
+    @ObjectIdColumn()
+    _id: string;
+
+    @Column()
+    author: User;
+
+    @Column()
+    description: string;
+
+    @Column({ type: 'timestamp' })
+    date: Date;
+}
